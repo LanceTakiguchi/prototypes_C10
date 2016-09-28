0a73a06d50015864655dd61cefa6de70b46201f2
ltakiguc <ltakiguc@uci.edu>
2016-09-28 11:29:17 -0700

    PHP Super Globals - Lance Takiguchi

---
+++ php_superglobals/index.php
@@ -0,0 +1,13 @@
+<!-- ***********
+Project Name: php_superglobals
+File Name: index.php
+Author: Lance Takiguchi
+Date: 09/13/2016 Time: 12:00
+Objective: Get familiar with superglobals
+Prompt: https://learning-fuze.github.io/prototypes_C10/#/PHP-Super-Globals
+ *********** -->
+<?php
+// <!-- Feature Set 1 -->
+print_r($_GET);
+var_dump($_POST);
+?>
