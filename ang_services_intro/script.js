43012e5f1a3a82c5c06a8462cdd603fec29a2440
ltakiguc <ltakiguc@uci.edu>
2016-10-05 17:46:04 -0700

    Feature Set 1 Complete

---
+++ ang_services_intro/script.js
@@ -0,0 +1,23 @@
+var app = angular.module("myMusic", []);
+
+app.controller("mainController", function($http, $log){
+
+    this.get_artist = function(artist){
+		var self_itunes = this;
+    	$http({
+    		url: "https://itunes.apple.com/search?term=" + artist + "&callback=JSON_CALLBACK", //self_itunes.url_constructor(artist_url), //TODO: Change to function
+            method: "jsonp",
+            cache: false
+    	})
+        .then(
+            function(success_response){
+                //$log.info("Artist", artist);
+                $log.info("Here is the response", success_response);
+                self_itunes.data = success_response;
+            },
+            function(fail_response){
+                self_itunes.data = fail_response;
+                $log.warn("$http Error: ", fail_response);
+            });
+	   };
+    });
